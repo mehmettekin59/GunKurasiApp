1e3fb93730cd3a63e4c2b743bb802f58725dcfac
mehmettekin59 <mehmettekin59@gmail.com>
2025-04-01 13:31:28 +0300

    Initial commit

---
+++ app/src/main/java/com/mehmettekin/gunkurasiapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.mehmettekin.gunkurasiapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mehmettekin.gunkurasiapp.ui.theme.GunKurasiAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GunKurasiAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
